6f7eb00350d77771cb6fbeb3e5c2210500c65ba7
kaihengann <annkaiheng@gmail.com>
2019-06-06 22:23:44 +0800

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: "", tasks: ["Buy milk", "Have dinner"] };
+  }
+
+  handleChange = e => this.setState({ input: e.target.value });
+
+  handleSubmit = e => {
+    if (e.key === "Enter" || e.type === "click") {
+      this.state.tasks.push(this.state.input);
+    }
+  };
+
+  render() {
+    const tasks = this.state.tasks.map(task => (<Task taskName={task} /> ));
+
+    return (
+      <React.Fragment>
+        <ul>
+          {tasks}
+        </ul>
+        <div>
+          <Form onChange={this.handleChange} onKeyDown={this.handleSubmit} />
+          <SubmitButton onClick={this.handleSubmit} />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
+
+const Task = ({ taskName }) => {
+  return (
+    <li>
+      <span>{taskName}</span>
+      <button className="delButton" />
+    </li>
+  );
+};
+
+const Form = ({ onKeyDown, onChange }) => {
+  return <input type="text" onChange={onChange} placeholder=">> Add Task" onKeyDown={onKeyDown} />;
+};
+
+const SubmitButton = ({ onClick }) => {
+  return <button type="submit" onClick={onClick} className="submitButton" />;
+};
+
+// Task -> taskname, delbutton
+// Form
+// Submit button
 
 export default App;
